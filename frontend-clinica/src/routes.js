d88a31d8e50d9675b1905adf0a49dc8dbe3bef3e
jleaal <jorgexleal@gmail.com>
2025-03-17 19:29:14 +0100

    Actualización de rutas

--- frontend-clinica/src/routes.js
+++ frontend-clinica/src/routes.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { useAuth } from "react-oidc-context";
 import Sidebar from "./components/Sidebar";
 import UserForm from "./components/UserForm";
 import BackButton from "./components/BackButton";
 import SearchComponent from "./components/SearchComponent";
 import Login from "./pages/Login";
-import AdminDashboard from "./pages/admin/AdminDashboard";
 import ManagePatients from "./pages/admin/ManagePatients";
 import ManageNutritionists from "./pages/admin/ManageNutritionists";
 import ManageAuxiliaries from "./pages/admin/ManageAuxiliaries";
-import NutritionistDashboard from "./pages/nutritionist/NutritionistDashboard";
 import MainPatientScreen from "./pages/patient/MainPatientScreen";
 import NutritionistSelection from "./pages/patient/NutritionistSelection";
 import TimeSelection from "./pages/patient/TimeSelection";
-import AuxiliarDashboard from "./pages/auxiliary/AuxiliarDashboard";
 import AppointmentConfirmation from "./pages/patient/AppointmentConfirmation";
 import ManageUsers from "./pages/admin/ManageUsers";
 import AdminAgenda from "./pages/admin/AdminAgenda";
+import NutritionistAgenda from "./pages/nutritionist/NutritionistAgenda";
+
 
 
 const AppRoutes = () => {
@@ -29,9 +28,9 @@ const AppRoutes = () => {
 
     const getUserType = () => {
         if (roles.includes("admin")) return "admin";
-        if (roles.includes("nutritionist")) return "nutritionist";
-        if (roles.includes("auxiliary")) return "auxiliary";
-        if (roles.includes("patient")) return "patient";
+        if (roles.includes("nutritionist")) return "nutritionists";
+        if (roles.includes("auxiliary")) return "auxiliaries";
+        if (roles.includes("patient")) return "patients";
         return null;
     };
 
@@ -51,31 +50,40 @@ const AppRoutes = () => {
             <Route path="/" element={<Login />} />
             <Route path="/:userType/profile" element={<ProtectedRoute allowedRoles={["admin", "nutritionist", "auxiliary", "patient"]}><UserForm isEditMode={true} userType={userType} /></ProtectedRoute>} />
 
-            <Route path="/admin" element={<ProtectedRoute allowedRoles={["admin"]}><AdminDashboard /></ProtectedRoute>} />
-            <Route path="/admin/agenda" element={<ProtectedRoute allowedRoles={["admin"]}><AdminAgenda /></ProtectedRoute>} />
-
-            {/* Gestión de Usuarios */}
-            <Route path="/admin/manage-users" element={<ProtectedRoute allowedRoles={["admin"]}><ManageUsers /></ProtectedRoute>}>
-                <Route path="patients" element={<ManagePatients />} />
-                <Route path="patients/create" element={<><BackButton defaultText="Gestión de Pacientes" /><UserForm userType="patient" /></>} />
-                <Route path="patients/edit/:id" element={<UserForm userType="patient" isEditMode={true} />} />
-                <Route path="patients/search" element={<><BackButton defaultText="Gestión de Pacientes" /><SearchComponent entityType="patients"/></>} />
-
-                <Route path="nutritionists" element={<ManageNutritionists />} />
-                <Route path="nutritionists/create" element={<><BackButton defaultText="Gestión de Nutricionistas" /><UserForm userType="nutritionist" /></>} />
-                <Route path="nutritionists/edit/:id" element={<UserForm userType="nutritionist" isEditMode={true} />} />
-                <Route path="nutritionists/search" element={<><BackButton defaultText="Gestión de Nutricionistas" /><SearchComponent entityType="nutritionists"/></>} />
-
-                <Route path="auxiliaries" element={<ManageAuxiliaries />} />
-                <Route path="auxiliaries/create" element={<><BackButton defaultText="Gestión de Auxiliares" /><UserForm userType="auxiliary" /></>} />
-                <Route path="auxiliaries/edit/:id" element={<UserForm userType="auxiliary" isEditMode={true} />} />
-                <Route path="auxiliaries/search" element={<><BackButton defaultText="Gestión de Auxiliares" /><SearchComponent entityType="auxiliaries"/></>} />
+            <Route path="/nutritionists" element={<Navigate to="/nutritionist/agenda" replace />} />  
+            <Route path="/nutritionist/agenda" element={<ProtectedRoute allowedRoles={["nutritionist"]}><NutritionistAgenda /></ProtectedRoute>} />
+            <Route path="/nutritionists/patients" element={<ProtectedRoute allowedRoles={["nutritionist"]}><ManagePatients userType="nutritionists" /></ProtectedRoute>}>
+                <Route path="create" element={<UserForm userType="patients" />} />
+                <Route path=":id" element={<><BackButton defaultText="Buscar Pacientes" /><UserForm userType="patients" isEditMode={true} /></>} />
+                <Route path="search" element={<SearchComponent entityType="patients" userType="nutritionists" />} />
             </Route>
 
+            <Route path="/auxiliaries" element={<Navigate to="/auxiliaries/agenda" replace />} />  
+            <Route path="/auxiliaries/agenda" element={<ProtectedRoute allowedRoles={["auxiliary"]}><AdminAgenda /></ProtectedRoute>} />
+            <Route path="/auxiliaries/patients" element={<ProtectedRoute allowedRoles={["auxiliary"]}><ManagePatients userType="auxiliaries" /></ProtectedRoute>}>
+                <Route path="create" element={<UserForm userType="patients" />} />
+                <Route path=":id" element={<><BackButton defaultText="Buscar Pacientes" /><UserForm userType="patients" isEditMode={true} /></>} />
+                <Route path="search" element={<SearchComponent entityType="patients" userType="auxiliaries" />} />
+            </Route>
 
-            {/* Dashboards */}
-            <Route path="/nutritionist" element={<ProtectedRoute allowedRoles={["nutritionist"]}><NutritionistDashboard /></ProtectedRoute>} />
-            <Route path="/auxiliary" element={<ProtectedRoute allowedRoles={["auxiliary"]}><AuxiliarDashboard /></ProtectedRoute>} />
+            <Route path="/admin" element={<Navigate to="/admin/agenda" replace />} />  
+            <Route path="/admin/agenda" element={<ProtectedRoute allowedRoles={["admin"]}><AdminAgenda /></ProtectedRoute>} />
+            <Route path="/admin/manage-users" element={<ProtectedRoute allowedRoles={["admin"]}><ManageUsers /></ProtectedRoute>}>
+                <Route path="patients" element={<ManagePatients userType="admin" />} />
+                <Route path="patients/create" element={<><BackButton defaultText="Gestión de Pacientes" /><UserForm userType="patients" /></>} />
+                <Route path="patients/:id" element={<><BackButton defaultText="Buscar Pacientes" /><UserForm userType="patients" isEditMode={true} /></>} />
+                <Route path="patients/search" element={<><BackButton defaultText="Gestión de Pacientes" /><SearchComponent entityType="patients" userType="admin"/></>} />
+
+                <Route path="nutritionists" element={<ManageNutritionists />} />
+                <Route path="nutritionists/create" element={<><BackButton defaultText="Gestión de Nutricionistas" /><UserForm userType="nutritionists" /></>} />
+                <Route path="nutritionists/:id" element={<><BackButton defaultText="Buscar Nutricionistas" /><UserForm userType="nutritionists" isEditMode={true} /></>} />
+                <Route path="nutritionists/search" element={<><BackButton defaultText="Gestión de Nutricionistas" /><SearchComponent entityType="nutritionists" userType="admin"/></>} />
+
+                <Route path="auxiliaries" element={<ManageAuxiliaries />} />
+                <Route path="auxiliaries/create" element={<><BackButton defaultText="Gestión de Auxiliares" /><UserForm userType="auxiliaries" /></>} />
+                <Route path="auxiliaries/:id" element={<><BackButton defaultText="Buscar Auxiliares" /><UserForm userType="auxiliaries" isEditMode={true} /></>} />
+                <Route path="auxiliaries/search" element={<><BackButton defaultText="Gestión de Auxiliares" /><SearchComponent entityType="auxiliaries" userType="admin"/></>} />
+            </Route>
 
             {/* Rutas de paciente anidadas */}
             <Route path="/patient" element={<ProtectedRoute allowedRoles={["patient"]}><MainPatientScreen /></ProtectedRoute>}>
