2d8735d8ba99cf3d680900b398381f311d8c81ec
jleaal <jorgexleal@gmail.com>
2025-03-17 19:27:31 +0100

    Creación de nuevos componentes: un modal para la creación de citas y interruptor para activar o desactivar pacientes y nutricionistas

---
+++ frontend-clinica/src/components/NewAppointmentModal.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import SearchComponent from "./SearchComponent";
+import moment from "moment";
+import "../styles/components/NewAppointmentModal.css";
+
+const NewAppointmentModal = ({ show, onClose, onConfirm, selectedTime }) => { 
+    const [selectedPatient, setSelectedPatient] = useState(null);
+
+    const handleConfirm = () => {
+        if (!selectedPatient) {
+            alert("Selecciona un paciente antes de continuar.");
+            return;
+        }
+
+        if (!selectedTime) {
+            alert("Error: No se ha definido una hora para la cita.");
+            return;
+        }
+
+        onConfirm(selectedPatient);
+        setSelectedPatient(null);
+        onClose();
+    };
+
+    return (
+        <Modal show={show} onHide={onClose} centered size="lg">
+            <Modal.Header closeButton>
+                <Modal.Title className="text-success text-align">Crear Nueva Cita</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <p className="appointment-text">
+                    Selecciona un paciente para la cita programada el <strong>
+                    {moment(selectedTime).format("DD/MM/YYYY")}</strong> a las <strong>{moment(selectedTime).format("HH:mm")}
+                    </strong>.
+                </p>
+                
+                <div className="table-container">
+                    <SearchComponent entityType="patients" onSelect={setSelectedPatient} selectedPatient={selectedPatient} showSelectButton={true} /> 
+                </div>
+
+            </Modal.Body>
+            <Modal.Footer> 
+                <Button className="cancel-button" onClick={onClose}>Cancelar</Button>
+                <Button 
+                    className={`confirm-button ${selectedPatient ? "active" : "disabled"}`} 
+                    onClick={handleConfirm} 
+                    disabled={!selectedPatient}
+                >
+                    Confirmar
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default NewAppointmentModal;
