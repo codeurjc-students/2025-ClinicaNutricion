04ebec7ba4889f546b5212804335a88fa4b56502
jleaal <jorgexleal@gmail.com>
2025-03-21 17:49:23 +0100

    Prueba de despliegue en develop v8

--- backend-clinica/src/main/java/com/jorgeleal/clinicanutricion/config/EnvLoader.java
+++ backend-clinica/src/main/java/com/jorgeleal/clinicanutricion/config/EnvLoader.java
@@ -4,7 +4,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvLoader {
     public static void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        String env = System.getenv("APP_ENV");
+        if (env == null) {
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        } else {
+            Dotenv dotenv = Dotenv.configure()
+                    .ignoreIfMissing()
+                    .load();
+        }
     }
 }
