6ed75a7a635f118529ff7463a6a0b6dd310603f0
jleaal <jorgexleal@gmail.com>
2025-05-15 14:07:45 +0200

    ci: redeploy backend

--- backend-clinica/src/test/java/com/jorgeleal/clinicanutricion/ClinicanutricionApplicationTests.java
+++ backend-clinica/src/test/java/com/jorgeleal/clinicanutricion/ClinicanutricionApplicationTests.java
@@ -2,8 +2,10 @@ package com.jorgeleal.clinicanutricion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClinicanutricionApplicationTests {
     @Test
     void contextLoads() {
